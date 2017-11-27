4004e1c6f72619b107e437134f0dd46bf8846ead
liujunqiang <liujunqiang@xunlei.com>
2017-11-27 13:45:13 +0800

    change: JWT token implement

--- lesson603/user-center/src/main/java/io/junq/examples/usercenter/spring/AuthorizationServerConfiguration.java
+++ lesson603/user-center/src/main/java/io/junq/examples/usercenter/spring/AuthorizationServerConfiguration.java
@@ -1,6 +1,7 @@
 package io.junq.examples.usercenter.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,7 +10,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -17,21 +19,34 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
     @Autowired
     private AuthenticationManager authenticationManager;
+    
+    @Value("${signing-key:oui214hmui23o4hm1pui3o2hp4m1o3h2m1o43}")
+    private String signingKey;
 
     public AuthorizationServerConfiguration() {
         super();
     }
     
     @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+    	final JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey(signingKey);
+        return jwtAccessTokenConverter;
+    }
+    
+    @Bean
     public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+        return new JwtTokenStore(accessTokenConverter());
     }
 
     // config
 
     @Override
     public void configure(final AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+        endpoints
+        .tokenStore(tokenStore())
+        .authenticationManager(authenticationManager)
+        .accessTokenConverter(accessTokenConverter());;
     }
 
     @Override
