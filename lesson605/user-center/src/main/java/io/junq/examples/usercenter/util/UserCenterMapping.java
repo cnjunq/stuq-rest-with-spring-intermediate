2ee3b84a5cba4955880a8861d4c444358be6eff2
liujunqiang <liujunqiang@xunlei.com>
2017-12-12 00:03:27 +0800

    new: lesson 605 initial code

---
+++ lesson605/user-center/src/main/java/io/junq/examples/usercenter/util/UserCenterMapping.java
@@ -0,0 +1,20 @@
+package io.junq.examples.usercenter.util;
+
+public final class UserCenterMapping {
+	
+    public static final String BASE = "/api/";
+	
+    public static final String USERS = "api/users";
+    public static final String PRIVILEGES = "api/privileges";
+    public static final String ROLES = "api/roles";
+    
+    public static final String AUTHENTICATION = "api/authentication";
+
+    public static final class Singular {
+
+        public static final String USER = "user";
+        public static final String PRIVILEGE = "privilege";
+        public static final String ROLE = "role";
+
+    }
+}
